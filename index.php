c0d6e0ed9f2db185cd9a02b13348e178080dd9f9
Abe Yang <abeyang@gmail.com>
2013-08-15 22:26:50 -0700

    checking in Han's code

--- index.php
+++ index.php
@@ -2,24 +2,19 @@
 
 <div class="container">
 
-	This is inside a normal container. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
-	
-</div>
-
-<p>Paragraph break! It's outside of the container!</p>
-
-<div class="container">
-
-	<div class="row">
-		<div class="col-lg-4">
-			This is inside a container with grids (this section takes up 4 columns). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
+	<div class="col-2">
+	</div>
+	<div class="col-8">
+		<div class="col-6">
 		</div>
-		<div class="col-lg-8">
-			Testing out <a href="http://fortawesome.github.io/Font-Awesome/icons/">Font Awesome</a> stuff:
-			<br /><i class="icon-dashboard icon-4x"></i> Dashboard icon! <i class="icon-camera-retro icon-4x"></i> Camera icon!
-			<i class="icon-plus-sign-alt icon-4x"></i> Cross icon!
+		<div class="col-6">
+		</div>
+		<div>
 		</div>
 	</div>
+	<div class="col-2">
+	</div>
+
 
 </div>
 
