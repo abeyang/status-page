14f598947e2e9a9cc5db499bd4cb1d770ef680ac
Abe Yang <abeyang@gmail.com>
2013-08-15 20:30:08 -0700

    updated with twitter toolkit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-status-page
-===========
+twitter-bootstrap-starter-kit
+=============================
 
-Simple status page
+Starter Kit that combines the best-in-class frameworks into repo
+
+## Getting Started
+
+Starter Kit includes:
+
++ [Bootstrap 3.0.0 RC2](http://twitter.github.io/bootstrap/)
++ [Font Awesome](http://fortawesome.github.io/Font-Awesome/)
++ [Open Sans](http://www.google.com/fonts#UsePlace:use/Collection:Open+Sans) // free font hosted by Google
++ [less.js](http://lesscss.org/) // for development purposes
+
+Disabled-by-default-but-it's-there-for-ya:
+
++ [jquery.js](http://jquery.com/)
++ [Kirby Tookit (PHP)](http://toolkit.getkirby.com/)
+
+This also includes a base index.php file along with its own _header.php and _footer.php.
+Controls are provided to easily flip between development (using .less) and production modes (using .css).
+
+## Modifying Files
+
+The assets folder is primarily separated by frameworks. Thus, whenever there's an update (to Twitter Bootstrap or Font Awesome), you can just swap out the entire directories without worrying about cross-dependencies.
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php require_once('_header.php'); ?>
+
+<div class="container">
+
+	This is inside a normal container. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
+	
+</div>
+
+<p>Paragraph break! It's outside of the container!</p>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-lg-4">
+			This is inside a container with grids (this section takes up 4 columns). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
+		</div>
+		<div class="col-lg-8">
+			Testing out <a href="http://fortawesome.github.io/Font-Awesome/icons/">Font Awesome</a> stuff:
+			<br /><i class="icon-dashboard icon-4x"></i> Dashboard icon! <i class="icon-camera-retro icon-4x"></i> Camera icon!
+			<i class="icon-plus-sign-alt icon-4x"></i> Cross icon!
+		</div>
+	</div>
+
+</div>
+
+<?php require_once('_footer.php'); ?>
